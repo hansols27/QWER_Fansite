d9abd5267b5c013f8beb3a2693d91a3a8075998d
hdy86 <dyhong86@naver.com>
2025-07-26 00:04:56 +0900

    layout & responsive css

--- Front_end/src/App.tsx
+++ Front_end/src/App.tsx
@@ -1,5 +1,6 @@
 import { Router } from './routes/Router';
 import './styles/global.css';
+import './styles/section.css';
 
 const App = () => {
   return <Router />;
--- Front_end/src/routes/Router.tsx
+++ Front_end/src/routes/Router.tsx
@@ -1,12 +1,38 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import Footer from '@/components/layout/Footer';
+import Header from '@/components/layout/Header';
+import { socialLinks } from '@/constants/snsLinks';
+import { openInNewTab } from '@/utils/openInNewTab';
 
 export const Router = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>
+  <div id="wrap">
+    <BrowserRouter>
+      {/* 헤더, 사이드 네비게이션 */}
+      <Header />
+
+      {/* SNS 메뉴 */}
+      <aside className="sns-menu">
+        <ul>
+          {socialLinks.map(({ id, icon, url, type }) => (
+            <li
+              key={id}
+              className={type || ''}
+              onClick={() => openInNewTab(url)}
+              title={id}
+              style={{ cursor: 'pointer' }}
+            >
+              <img src={icon} alt={`${id} 아이콘`} width={32} height={32} />
+            </li>
+          ))}
+        </ul>
+      </aside>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  </div>
 );
