4cca8fc255a1a5f9260d435ce902bc8ebdadf633
hansols27 <tpfmgks@naver.com>
2025-07-28 09:07:19 +0900

    home modify

--- Front_end/src/pages/Home.tsx
+++ Front_end/src/pages/Home.tsx
@@ -1,39 +1,11 @@
 import React, { useState } from 'react';
-import YouTubeEmbed from '@/components/common/YouTubeEmbed';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import { videoIds } from '@/data/videoIds';
 
 const Home = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
-      {/* 유튜브 영상 및 썸네일 */}
-      <section className="youtube-section">
-        <div className="youtube-main-video">
-          <YouTubeEmbed videoId={videoIds[selectedIndex]} width="100%" />
-        </div>
 
-        <div className="youtube-thumbnails">
-          <Swiper spaceBetween={20} slidesPerView={4}>
-            {videoIds.map((id, idx) => (
-              <SwiperSlide key={`${id}-${idx}`}>
-                <div
-                  className={`youtube-thumbnail ${idx === selectedIndex ? 'active' : ''}`}
-                  onClick={() => setSelectedIndex(idx)}
-                  style={{ cursor: 'pointer' }}
-                >
-                  <img
-                    src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
-                    alt={`Video thumbnail ${idx + 1}`}
-                  />
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </section>
     </>
   );
 };
