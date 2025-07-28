dd0f59e4e9156da9d9e531251d16533c45bc27ef
hansol <tpfmgks@naver.com>
2025-07-28 22:58:13 +0900

    home.tsx modify

--- Front_end/src/pages/Home.tsx
+++ Front_end/src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import menu from '@/assets/images/main.webp';
 
 const Home = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
-
+      <main id="main-content">
+        <img src={menu} alt="main image" className="main-image" />
+      </main>
     </>
   );
 };
