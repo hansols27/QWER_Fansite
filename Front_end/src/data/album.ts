b7d5c60347118b6b3f9b9cfd8880dc670915b258
hansol <tpfmgks@naver.com>
2025-08-07 21:28:38 +0900

    discography add

---
+++ Front_end/src/data/album.ts
@@ -0,0 +1,51 @@
+export interface Album {
+  title: string;
+  date: string;
+  imgUrl: string;
+  link: string;
+}
+
+export const albums: Album[] = [
+  {
+    title: 'Maybe Tomorrow',
+    date: '2025-05-07',
+    imgUrl:
+      'https://lv2-cdn.azureedge.net/day6/ba90fc52fdd14191a35a0aa02d46063c-%EA%B3%B5%ED%99%88%EC%9A%A9%20%EC%95%A8%EB%B2%94%20%EC%BB%A4%EB%B2%84.jpg',
+    link: '/Default/DiscographyView?AamSeq=275&AmSeq=0&PgIndex=0',
+  },
+  {
+    title: '30',
+    date: '2024-11-05',
+    imgUrl:
+      'https://lv2-cdn.azureedge.net/day6/5840883eab6d4956867aa513487215d6-SUNGJIN_30_%E1%84%8B%E1%85%A9%E1%86%AB%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%86%AB%20%E1%84%8F%E1%85%A5%E1%84%87%E1%85%A5[%E1%84%8E%E1%85%AC%E1%84%8C%E1%85%A9%E1%86%BC].jpg',
+    link: '/Default/DiscographyView?AamSeq=256&AmSeq=0&PgIndex=0',
+  },
+  {
+    title: 'Band Aid',
+    date: '2024-09-02',
+    imgUrl:
+      'https://lv2-cdn.azureedge.net/day6/e3deec5cdb394b50a6d0f3697da9e728-%EA%B3%B5%ED%99%88%EC%9A%A9%EC%95%A8%EB%B2%94%EC%BB%A4%EB%B2%84.jpg',
+    link: '/Default/DiscographyView?AamSeq=243&AmSeq=0&PgIndex=0',
+  },
+  {
+    title: 'Fourever',
+    date: '2024-03-18',
+    imgUrl:
+      'https://lv2-cdn.azureedge.net/day6/c6ad7014600340ee95c2971f77da3e2b-240317_DAY6_MN8_%EC%98%A8%EB%9D%BC%EC%9D%B8%EC%BB%A4%EB%B2%84[%EC%B5%9C%EC%A2%85]_%EC%88%98%EC%A0%95.jpg',
+    link: '/Default/DiscographyView?AamSeq=228&AmSeq=0&PgIndex=0',
+  },
+  {
+    title: 'Letters with notes',
+    date: '2023-09-04',
+    imgUrl:
+      'https://lv2-cdn.azureedge.net/day6/1d53b6ff27fd447f99bcda5a037e07c6-Young%20K_Letters%20with%20notes.jpg',
+    link: '/Default/DiscographyView?AamSeq=207&AmSeq=0&PgIndex=0',
+  },
+  {
+    title: 'Pilmography',
+    date: '2022-02-07',
+    imgUrl:
+      'https://lv2-cdn.azureedge.net/day6/7e73aa6ac9b9421ba7c4451ae7c7aea1-4a8e327c48614785896e10e18e85d3bc-220107_%EC%9B%90%ED%95%84_%EC%98%A8%EB%9D%BC%EC%9D%B8%EC%BB%A4%EB%B2%84[%EC%B5%9C%EC%A2%85].jpg',
+    link: '/Default/DiscographyView?AamSeq=159&AmSeq=0&PgIndex=0',
+  },
+];
--- Front_end/src/pages/Discography.tsx
+++ Front_end/src/pages/Discography.tsx
@@ -1,9 +1,83 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { albums } from '@/data/album';
+import '@/ui/discography.css';
 
-export default function Discography() {
+import more_view from '@/assets/icons/more_view.png';
+import btn_prev from '@/assets/icons/bg-btn-prev.png';
+import btn_next from '@/assets/icons/bg-btn-next.png';
+
+interface DiscographyProps {
+  currentPage: number;
+  totalPages: number;
+}
+
+const Discography: React.FC<DiscographyProps> = ({
+  currentPage,
+  totalPages,
+}) => {
   return (
-    <div id="wrap">
+    <div className="container">
+      {/* Side */}
+      <div id="side">
+        <div className="side2">
+          02
+          <span className="s_line"></span>
+          DISCOGRAPHY
+        </div>
+      </div>
+      <div className="cont discography_view wow fadeInUp" data-wow-delay="0.2s">
+        <div className="title">DISCOGRAPHY</div>
 
+        <div className="new_release_list">
+          {albums.map((album, index) => (
+            <div className="new_album_cont" key={index}>
+              <a href={album.link}>
+                <div className="img">
+                  <img
+                    alt={album.title}
+                    src={album.imgUrl}
+                    width={344}
+                    height={344}
+                  />
+                  <div className="list-hover">
+                    <img alt="자세히보기" src={more_view} />
+                  </div>
+                </div>
+                <div className="txt">
+                  <p>
+                    {album.title}
+                    <span>{album.date}</span>
+                  </p>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+
+        <div className="page-btn-box">
+          <button
+            type="button"
+            className="prev-btn"
+            disabled={currentPage <= 1}
+          >
+            <img alt="이전" src={btn_prev} />
+            이전
+          </button>
+          <span className="page-number">
+            <strong>{currentPage}</strong> / <em>{totalPages}</em>
+          </span>
+          <button
+            type="button"
+            className="next-btn"
+            disabled={currentPage >= totalPages}
+          >
+            <img alt="이후" src={btn_next} />
+            이후
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Discography;
