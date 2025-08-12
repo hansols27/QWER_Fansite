4e858719d38a0776dc35fb714a7e3cb4eef9ab82
hansol <tpfmgks@naver.com>
2025-08-12 21:40:10 +0900

    css modify

--- Front_end/src/pages/Discography.tsx
+++ Front_end/src/pages/Discography.tsx
@@ -38,59 +38,59 @@ export default function Discography() {
       </div>
 
       {/* Main */}
-        <div className="title">DISCOGRAPHY</div>
+      <div className="title">DISCOGRAPHY</div>
 
-        {/* 앨범 목록 */}
-        <div className="new_release_list">
-          {currentAlbums.map((album, index) => (
-            <div className="new_album_cont" key={index}>
-              <a href={album.link}>
-                <div className="img">
-                  <img
-                    alt={album.title}
-                    src={album.imgUrl}
-                    width={340}
-                    height={340}
-                  />
-                  <div className="list-hover">
-                    <img alt="자세히보기" src={more_view} />
-                  </div>
+      {/* 앨범 목록 */}
+      <div className="new_release_list">
+        {currentAlbums.map((album, index) => (
+          <div className="cont" key={index}>
+            <a href={album.link}>
+              <div className="img">
+                <img
+                  alt={album.title}
+                  src={album.imgUrl}
+                  width={340}
+                  height={340}
+                />
+                <div className="list-hover">
+                  <img alt="자세히보기" src={more_view} />
                 </div>
-                <div className="txt">
-                  <p>
-                    {album.title}
-                    <span>{album.date}</span>
-                  </p>
-                </div>
-              </a>
-            </div>
-          ))}
-        </div>
-
-        {/* Pagination */}
-        <div className="page-btn-box">
-          <button
-            type="button"
-            className="prev-btn"
-            onClick={goPrev}
-            disabled={currentPage <= 1}
-          >
-            <img alt="이전" src={btn_prev} />
-            이전
-          </button>
-          <span className="page-number">
-            <strong>{currentPage}</strong> / <em>{totalPages}</em>
-          </span>
-          <button
-            type="button"
-            className="next-btn"
-            onClick={goNext}
-            disabled={currentPage >= totalPages}
-          >
-            <img alt="이후" src={btn_next} />
-            이후
-          </button>
-        </div>
+              </div>
+              <div className="txt">
+                <p>
+                  {album.title}
+                  <span>{album.date}</span>
+                </p>
+              </div>
+            </a>
+          </div>
+        ))}
       </div>
+
+      {/* Pagination */}
+      <div className="page-btn-box">
+        <button
+          type="button"
+          className="prev-btn"
+          onClick={goPrev}
+          disabled={currentPage <= 1}
+        >
+          <img alt="이전" src={btn_prev} />
+          이전
+        </button>
+        <span className="page-number">
+          <strong>{currentPage}</strong> / <em>{totalPages}</em>
+        </span>
+        <button
+          type="button"
+          className="next-btn"
+          onClick={goNext}
+          disabled={currentPage >= totalPages}
+        >
+          <img alt="이후" src={btn_next} />
+          이후
+        </button>
+      </div>
+    </div>
   );
 }
