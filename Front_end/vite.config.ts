dc966af75c702d7796672651cec1b7b5c366d98e
hansols27 <tpfmgks@naver.com>
2025-08-21 10:04:08 +0900

    modify

--- Front_end/vite.config.ts
+++ Front_end/vite.config.ts
@@ -13,4 +13,12 @@ export default defineConfig({
       { find: '@api', replacement: '/src/api' },
     ],
   },
+  optimizeDeps: {
+    include: ['react-image-lightbox'], // Vite가 빌드 전에 ESM으로 변환하도록 강제
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/], // CommonJS 패키지도 빌드에서 처리
+    },
+  },
 });
